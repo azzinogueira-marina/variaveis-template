8eb648b4f22d7bb1fca226ccdc9019ceeab91d37
Gilson-barbosa-dev <gilsonbarbosa.s@gmail.com>
2023-01-31 18:48:59 -0300

    Exercicios de casa

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
+//Exercicio 1
+
+/*const nome = prompt('Qual é o seu nome?')
+let idade = Number(prompt('Qual é sua idade? '))
+let temCachorro = Boolean(prompt('Você tem cachorro? '))
+
+console.log(typeof(nome))
+console.log(typeof(idade))
+console.log(typeof(temCachorro))
+
+console.log('Meu nome é ', nome, 'tenho ',idade,'anos e',temCachorro,'Cachorro')*/
+
+// Exercicio 2
+
+/*let a = 10
+let b = 15
+let c = null
+
+c = a
+a = b
+
+console.log('esse é o valor da variável a ',a)
+console.log('esse é o valor da variável b ',c)*/
+
+// Exercicio 3
+
+const nomeCompleto = prompt('Qual é seu nome completo?')
+const dNascimento  = Number(prompt('Qual é sua data de nascimento?'))
+const cpf          = Number(prompt('Preencha o seu CPF'))
+let comissao       = Number(prompt('Recebe comissão? '))
+let admissao       = Number(prompt('Informe o ano de admissão'))
+let salario        = Number(prompt('Qual é o seu salario atual?'))
+let endereco       = prompt('Qual o seu endereço?')
+let escolaridade   = prompt('Qual a sua escolaridade?')
+let filmes         = prompt('Quantos filmes já assistiu?')
+let cargo          = prompt('Qual é o seu cargo atual?')
+let cnh            = Boolean(prompt('Possui CNH?'))
+
+console.log('Tipo nome completo',typeof nomeCompleto)
+console.log('Tipo data de nascimento',typeof dNascimento)
+console.log('Tipo de CPF',typeof cpf)
+console.log('Tipo de comissao',typeof comissao)
+console.log('Tipo de admissao',typeof admissao)
+console.log('Tipo de salario',typeof salario)
+console.log('Tipo de endereco',typeof endereco)
+console.log('Tipo de escolaridade',typeof escolaridade)
+console.log('Tipo de filmes',typeof filmes)
+console.log('Tipo de cargos',typeof cargo)
+console.log('Tipo de CNH',typeof cnh)
+
+console.log('Nome: ',nomeCompleto,'\n','Data de Nascimento: ',dNascimento,'\n','CPF: ',cpf,'\n','Comissão: ',comissao,'%','\n','Admissão: ',admissao,'\n','Salário: ',salario,'\n','Endereço: ',endereco,'\n','Escolaridade: ',escolaridade,'\n','Filmes: ',filmes,'\n','Cargo: ',cargo,'\n','CNH: ',cnh)
+
+
+
+
+
 
